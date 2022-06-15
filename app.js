a1de2d38ecf2876819bb8461bab70070eb162228
MATTHEW CHRISTENSEN <matthewbchristensen@comcast.net>
2022-06-14 23:45:15 -0600

    finished individual assignment for week 3

--- app.js
+++ app.js
@@ -2,9 +2,12 @@ const express = require('express');
 const app = express();
 const port = process.env.PORT || 3000;
 const mongodb = require('./db/connect');
+const bodyParser = require('body-parser');
 if (process.env.NODE_ENV !== 'production') {
   require('dotenv').config();
 }
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
 
 app.use('/', require('./routes'));
 
--- controllers/contacts.js
+++ controllers/contacts.js
@@ -11,7 +11,6 @@ const getAllContacts = async (req, res, next) => {
 
 const getContactByID = async (req, res, next) => {
   const userId = new ObjectId(req.params.id);
-  console.log("userId: ", userId);
   const result = await mongodb.getDb().db().collection('contacts').find({ _id: userId });
   result.toArray().then((lists) => {
     res.setHeader('Content-Type', 'application/json');
@@ -19,7 +18,51 @@ const getContactByID = async (req, res, next) => {
   });
 };
 
-module.exports = { getAllContacts, getContactByID };
+const createNewClient = async (request, response) => {
+  const newUser = request.body;
+  console.log("newUser: ", newUser);
+  const result = await mongodb.getDb().db().collection('contacts').insertOne(newUser);
+  if (response.acknowledged) {
+    console.log("result: ", result.insertedId);
+    response.setHeader('Content-Type', 'application/json');
+    response.status(201).json(result);
+  } else {
+    
+    response.setHeader('Content-Type', 'application/json');
+    response.status(500).json(response.error || "Some error occured while creating the contact.");
+  }
+};
+
+const updateContact = async (req, res) => {
+  const userId = new ObjectId(req.params.id);
+  // be aware of updateOne if you only want to update specific fields
+  const contact = {
+    firstName: req.body.firstName,
+    lastName: req.body.lastName,
+    email: req.body.email,
+    favoriteColor: req.body.favoriteColor,
+    birthday: req.body.birthday
+  };
+  const response = await mongodb.getDb().db().collection('contacts').replaceOne({ _id: userId }, contact);
+  console.log(response);
+  if (response.modifiedCount > 0) {
+    res.status(204).send();
+  } else {
+    res.status(500).json(response.error || 'Some error occurred while updating the contact.');
+  }
+};
+
+const deleteContact = async (req, res) => {
+  const userId = new ObjectId(req.params.id);
+  const response = await mongodb.getDb().db().collection('contacts').remove({ _id: userId }, true);
+  console.log(response);
+  if (response.deletedCount > 0) {
+    res.status(204).send();
+  } else {
+    res.status(500).json(response.error || 'Some error occurred while deleting the contact.');
+  }
+};
+module.exports = { getAllContacts, getContactByID, createNewClient, updateContact, deleteContact };
 
 /*.catch((err) => {
   callback(err);
